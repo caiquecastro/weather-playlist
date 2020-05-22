6e08928704888d8a5991e3f317ed882a5df70781
Caique Castro <castro.caique@gmail.com>
2020-05-22 14:55:56 -0300

    Add spotify service class

--- app/main.py
+++ app/main.py
@@ -1,37 +1,7 @@
-import spotipy
-import requests
 from fastapi import FastAPI
 from app.core.config import settings
-from spotipy.oauth2 import SpotifyClientCredentials
 from app.services.openweather import openweather_service
-
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-
-
-def get_track_name(item):
-    return item['track']['name']
-
-
-def get_temperature_playlist(temperature):
-    try:
-        playlists = get_temperature_playlists(temperature)
-
-        playlist_tracks = sp.playlist_tracks(
-            playlists['playlists']['items'][0]['id']
-        )
-
-        return list(map(get_track_name, playlist_tracks['items']))
-    except Exception as er:
-        return []
-
-def get_temperature_playlists(temperature):
-    if temperature > 25:
-        return sp.category_playlists('pop')
-    
-    if temperature >= 10:
-        return sp.category_playlists('rock')
-
-    return sp.category_playlists('classical')
+from app.services.spotify import spotify_service
 
 
 app = FastAPI(title=settings.PROJECT_NAME)
@@ -41,7 +11,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 def city_playlists(city):
     city_temperature = openweather_service.get_temperature(city)
 
-    playlist = get_temperature_playlist(city_temperature)
+    playlist = spotify_service.get_playlist_for_temperature(city_temperature)
     
     return {
         'city': city,
---
+++ app/services/spotify.py
@@ -0,0 +1,32 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+class SpotifyService:
+    def __init__(self):
+        self.client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+    def _get_track_name(self, item):
+        return item['track']['name']
+
+    def _get_temperature_playlists(self, temperature):
+        if temperature > 25:
+            return self.client.category_playlists('pop')
+        
+        if temperature >= 10:
+            return self.client.category_playlists('rock')
+
+        return self.client.category_playlists('classical')
+
+    def get_playlist_for_temperature(self, temperature):
+        try:
+            playlists = self._get_temperature_playlists(temperature)
+
+            playlist_tracks = self.client.playlist_tracks(
+                playlists['playlists']['items'][0]['id']
+            )
+
+            return list(map(get_track_name, playlist_tracks['items']))
+        except Exception as er:
+            return []
+
+spotify_service = SpotifyService()
