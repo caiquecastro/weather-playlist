3a19e86e2c2cbf697d07da49ae560fe138e1a303
Caique Castro <castro.caique@gmail.com>
2020-05-21 21:53:11 -0300

    First commit
    Bootstrap api
    Setup docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.7
+
+COPY requirements.txt /app
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./app /app
---
+++ app/main.py
@@ -0,0 +1,55 @@
+import spotipy
+import requests
+from fastapi import FastAPI
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+def get_city_temperature(city):
+    app_id = ''
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={app_id}&units=metric'
+
+    response = requests.get(url)
+
+    response_json = response.json()
+
+    return response_json['main']['temp']
+
+
+def get_track_name(item):
+    return item['track']['name']
+
+
+def get_temperature_playlist(temperature):
+    playlists = get_temperature_playlists(temperature)
+
+    first_playlist = playlists['playlists']['items'][0]
+
+    playlist_tracks = sp.playlist_tracks(first_playlist['id'])
+
+    return list(map(get_track_name, playlist_tracks['items']))
+
+def get_temperature_playlists(temperature):
+    if temperature > 25:
+        return sp.category_playlists('pop')
+    
+    if temperature >= 10:
+        return sp.category_playlists('rock')
+
+    return sp.category_playlists('classical')
+
+
+app = FastAPI()
+
+
+@app.get('/cities/{city}/playlists')
+def city_playlists(city):
+    city_temperature = get_city_temperature(city)
+
+    playlist = get_temperature_playlist(city_temperature)
+    
+    return {
+        'city': city,
+        'temperature': city_temperature,
+        'playlist': playlist,
+    }
