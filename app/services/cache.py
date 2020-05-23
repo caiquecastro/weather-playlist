80fd795d1acf1ecebcee516de888510ac6b07544
Caique Castro <castro.caique@gmail.com>
2020-05-22 23:20:40 -0300

    Register access

--- app/services/cache.py
+++ app/services/cache.py
@@ -1,5 +1,10 @@
+import json
 import redis
+import logging
 from ..core.config import settings
+
+
+logger = logging.getLogger(__name__)
 
 
 class Cache:
@@ -7,10 +12,39 @@ class Cache:
         self.redis = redis.from_url(settings.REDIS_URL)
 
     def get(self, key):
-        return self.redis.get(key)
+        logger.debug(f'Fetching cached value for {key}')
+
+        value = self.redis.get(key)
+
+        if value is None:
+            return None
+
+        return json.loads(value)
 
     def set(self, key, value, expire=None):
-        return self.redis.set(key, value, ex=expire)
+        logger.debug(f'Saving cached value for {key}')
+
+        return self.redis.set(key, json.dumps(value), ex=expire)
+
+
+    def increment_by(self, key, amount, value):
+        return self.redis.zincrby(key, amount, value)
+
+    def get_range(self, key, start, end, with_scores=True):
+        result = self.redis.zrevrange(
+            key,
+            start,
+            end,
+            withscores=with_scores
+        )
+
+        if not with_scores:
+            return result
+
+        return [{
+            'key': key,
+            'value': value,
+        } for key, value in result]
 
 
 cache = Cache()
