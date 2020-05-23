c77c5e64db8a065da582b6c1d6b321e7395461ab
Caíque de Castro Soares da Silva <castro.caique@gmail.com>
2020-05-23 11:20:40 -0300

    Handle external service error (#8)

--- app/api.py
+++ app/api.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from app.errors import AppError
 from app.core.config import settings
 from app.services.stats import stats_service
 from app.services.spotify import spotify_service
@@ -7,6 +9,15 @@ from app.services.openweather import openweather_service
 
 def create_app():
     app = FastAPI(title=settings.PROJECT_NAME)
+
+    @app.exception_handler(AppError)
+    async def app_exception_handler(request: Request, exc: AppError):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={
+                'detail': exc.message,
+            }
+        )
 
     @app.get('/stats')
     def city_stats():
