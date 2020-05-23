283f0de209533e5bff651ffe5e9cbd99da9e27d0
Caíque de Castro Soares da Silva <castro.caique@gmail.com>
2020-05-22 23:16:58 -0300

    Cache requests to spotify service (#6)

--- app/services/spotify.py
+++ app/services/spotify.py
@@ -1,5 +1,11 @@
+import json
 import spotipy
+import logging
+from .cache import cache
 from spotipy.oauth2 import SpotifyClientCredentials
+
+
+logger = logging.getLogger(__name__)
 
 
 class SpotifyService:
@@ -7,18 +13,40 @@ class SpotifyService:
         credentials = SpotifyClientCredentials()
         self.client = spotipy.Spotify(client_credentials_manager=credentials)
 
-    def _get_temperature_playlists(self, temperature):
+        self.cache_prefix = 'playlist:'
+
+    def _get_temperature_genre(self, temperature):
         if temperature > 25:
-            return self.client.category_playlists('pop')
+            return 'pop'
 
         if temperature >= 10:
-            return self.client.category_playlists('rock')
+            return 'rock'
 
-        return self.client.category_playlists('classical')
+        return 'classical'
 
     def get_playlist_for_temperature(self, temperature):
+        genre = self._get_temperature_genre(temperature)
+
+        cache_key = f'{self.cache_prefix}{genre}'
+        cached_playlist = cache.get(cache_key)
+
+        if cached_playlist is not None:
+            return cached_playlist
+
+        playlist = self.fetch_playlist_for_genre(genre)
+
+        self.cache_playlist(cache_key, playlist)
+
+        return playlist
+
+    def cache_playlist(self, cache_key, playlist):
+        cache.set(cache_key, playlist, expire=3600)
+
+    def fetch_playlist_for_genre(self, genre):
+        logger.info(f'Fetching playlist for {genre}')
+
         try:
-            playlists = self._get_temperature_playlists(temperature)
+            playlists = self.client.category_playlists(genre)
 
             playlist_tracks = self.client.playlist_tracks(
                 playlists['playlists']['items'][0]['id']
@@ -26,10 +54,9 @@ class SpotifyService:
 
             return [{
                 'name': item['track']['name'],
-                'track': item['track'],
             } for item in playlist_tracks['items']]
         except Exception as er:
-            return []
+            return None
 
 
 spotify_service = SpotifyService()
